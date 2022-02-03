4fde3fc73b81621f016d56614537e26a8bf23f35
Nikita12G <gulyi08@mail.ru>
2022-02-03 14:38:22 +0700

    Initial Commit

---
+++ RxSwiftTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftTableView
+//
+//  Created by Никита Гуляев on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
